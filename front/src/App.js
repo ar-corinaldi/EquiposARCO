85c6ae047e342455ab10dd938166895ab54590a3
Diego <diego_gomezpolo@outlook.com>
2020-06-22 18:19:24 -0500

    Sidebar v0.1

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Container from "react-bootstrap/Container";
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 function App() {
-  return <div>
-    <Navbar></Navbar>
-  </div>;
+  return (
+      <div id="contenido">
+        <Navbar></Navbar>
+        <Row>
+          <Col md={2} id="sidebar-column">
+            <Sidebar />
+          </Col>
+          <Col id="content-column"><p>Acá va el contenido</p>
+          </Col>
+        </Row>  
+      </div>
+  );
 }
 
 export default App;

368aa2ca2bff3fa66e488fdef127b5beeae6e13a
Allan Corinaldi <allan9899@gmail.com>
2020-06-23 11:49:19 -0500

    Equipo Component y quitando serviceWorker y tests

--- front/src/App.js
+++ front/src/App.js
@@ -1,38 +1,25 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+// Container
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import Equipo from "./components/Equipo";
+
+// Bootstrap
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+
 function App() {
   return (
     <div id="contenido">
       <Navbar></Navbar>
-      <Row>
-        <Col md={3} lg={2} id="sidebar-column" className='d-none d-md-block'>
+      <Row id="rowWrapper">
+        <Col md={3} lg={2} id="sidebar-column" className="d-none d-md-block">
           <Sidebar />
         </Col>
         <Col id="content-column">
-          <p>Acá va el contenido</p>
-          <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
-          </p>
-          <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
-          </p>
+          <Equipo />
         </Col>
       </Row>
     </div>
--- front/src/components/Equipo.js
+++ front/src/components/Equipo.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import EquipoFilter from "./EquipoFilter";
+import EquipoTable from "./EquipoTable";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+let equiposTmp = [
+  {
+    _id: "1",
+    nombre: "mezcladora",
+    especificacion: "0.5 bulto",
+    tipoCobro: "Unid/ dia + IVA",
+    alquiler: "32000",
+    reposicion: "2354000",
+    categoria: "maquinaria pesada",
+  },
+  {
+    _id: "2",
+    nombre: "vibradora",
+    especificacion: "0.5 bulto",
+    tipoCobro: "Unid/ dia + IVA",
+    alquiler: "32000",
+    reposicion: "2354000",
+    categoria: "maquinaria liviana",
+  },
+  {
+    _id: "3",
+    nombre: "allanadora",
+    especificacion: "0.5 bulto",
+    tipoCobro: "Unid/ dia + IVA",
+    alquiler: "32000",
+    reposicion: "2354000",
+    categoria: "maquinaria liviana",
+  },
+];
 
 function Equipo(props) {
-  return <div></div>;
+  const [equipos, setEquipos] = useState(equiposTmp);
+  const [filterText, setFilterText] = useState("");
+
+  useEffect(() => {
+    // El fetch al equipo viene ordenado por categoria
+    // const res = await fetch("/equipos");
+    // const newEquipos = await res.json();
+
+    setEquipos(equiposTmp);
+    console.log("filter", filterText);
+  }, [filterText]);
+
+  return (
+    <Router>
+      <Container>
+        <Row>
+          <EquipoFilter filterText={filterText} setFilterText={setFilterText} />
+        </Row>
+        <Row>
+          <Switch>
+            <Route path="/" exact>
+              <EquipoTable equipos={equipos} filterText={filterText} />
+            </Route>
+          </Switch>
+        </Row>
+      </Container>
+    </Router>
+  );
 }
 
 export default Equipo;

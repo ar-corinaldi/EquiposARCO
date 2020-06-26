c9ca179c3868b5f6f3744eca04c9ad85ad526a6a
Diego <diego_gomezpolo@outlook.com>
2020-06-25 19:36:17 -0500

    crud empleados

--- back/src/routers/empleados.js
+++ back/src/routers/empleados.js
@@ -3,11 +3,93 @@ const mongoose = require("mongoose");
 const Empleado = require("../models/empleado-model");
 const router = new express.Router();
 
+/**
+ * Mete un empleado en la base de datos
+ */
+router.post("", async (req, resp) => {
+    try {
+        const empleado = new Empleado(req,body);
+        await empleado.save();
+        resp.status(201).send(empleado);
+        
+    } catch (error) {
+        resp.status(400).send(error);
+    }
+}
+);
 
 
 
+/**
+ *  Devuelve todos los empleados de la DB
+ */
+router.get("", async (req, res) => {
+    try {
+      const empleados = await Empleado.find({});
+      res.send(empleados);
+    } catch (e) {
+      res.status(500).send();
+    }
+  });
 
 
+
+  /**
+   * Devuelve la información del empleado con el id especificado
+   */
+  router.get("/:id", async(req,res) =>{
+      try {
+        const empleados = await Empleado.findById(req.params.id);
+        if(!empleado){
+            res.status(404).send("No hubo coincidencia");
+        }
+        res.status(200).send(equipo);
+          
+      } catch (error) {
+        res.status(500).send();   
+      }
+  });
+
+
+
+  /**
+ *  Modifica un empleado con el id especificado
+ */
+router.patch("/:id", async (req, res) => {
+    // Se pueden pasar por parametro los campos no modificables
+    try {
+      if (!Empleado.fieldsNotAllowedUpdates(req.body)) {
+        return res.status(400).send({ error: "Invalid updates" });
+      }
+      ("console.log(req.body)");
+      const empleado = await Empleado.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      if (!empleado) {
+        return res.status(404).send();
+      }
+      res.send(empleado);
+    } catch (e) {
+      res.status(400).send(e);
+    }
+  });
+
+
+  /**
+ * Elimina un empleado
+ */
+router.delete("/:id", async (req, res) => {
+    try {
+      const empleado = await Empleado.findByIdAndDelete(req.params.id);
+      if (!empleado) {
+        return res.status(404).send();
+      }
+      res.send(empleado);
+    } catch (error) {
+      res.status(500).send();
+    }
+  });
 
 
 module.exports = router;

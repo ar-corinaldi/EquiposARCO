ac4a1f31d675e30aa94707328b468047d108dcdf
Isabela <im.sarmieento@uniandes.edu.co>
2020-06-26 17:27:09 -0500

    First commit
    nota de inventario basics

---
+++ back/src/models/notaInventario-model.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+/*
+ * Definición del modelo con sus propiedades
+ */
+const notaInventarioSchema = new Schema({
+  descripcion: {
+    type: String,
+    trim: true,
+    required: true,
+    lowercase: true,
+  },
+  cantidad: {
+    type: Number,
+    default: 1,
+  },
+  fecha: {
+    type: Date,
+    required: true,
+  },
+});
+
+const NotaInventario = mongoose.model("NotaInventario", notaInventarioSchema);
+
+// // Arreglo de los campos que no se pueden modificar
+const noUpdatable = [fecha, cantidad, descripcion];
+
+// /**
+//  * Funcion para revisar que las modificiaciones son validas
+//  * @param body: Corresponde a los campos que se van a actualizar
+//  * @returns retorna true si todos los campos que se actualizan se pueden,
+//  *  retorna false en caso contrario.
+//  */
+Precio.fieldsNotAllowedUpdates = (body) => {
+  const updates = Object.keys(body);
+
+  // Sirve para obtener los campos del modelo
+  let allowedUpdates = Object.keys(Precio.schema.paths);
+
+  // Deja los campos que no queremos moficiar
+  allowedUpdates = allowedUpdates.filter(
+    (update) => !noUpdatable.includes(update)
+  );
+  const isValidOp = updates.every((update) => allowedUpdates.includes(update));
+  console.log(updates);
+  return isValidOp;
+};
+
+module.exports = NotaInventario;

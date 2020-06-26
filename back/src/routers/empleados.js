8bfa7c8fede3a23b7e7598d5d71f683da891ecda
Diego <diego_gomezpolo@outlook.com>
2020-06-25 19:02:51 -0500

    modelo de empleado

---
+++ back/src/routers/empleados.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Empleado = require("../models/empleado-model");
+const router = new express.Router();
+
+
+
+
+
+
+
+
+module.exports = router;

b5d18d133802d6eb5e7a7751db60a6bfdca8055b
Isabela <im.sarmieento@uniandes.edu.co>
2020-06-28 19:30:04 -0500

    Proveedor creacion

---
+++ back/src/models/proveedor-model.js
@@ -0,0 +1,80 @@
+const mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+const proveedorSchema = new Schema({
+  tipoDocumento: {
+    type: String,
+    trim: true,
+    required: true,
+    lowercase: true,
+  },
+  numeroDocumento: {
+    type: String,
+    trim: true,
+    required: true,
+    lowercase: true,
+  },
+  direcciones: {
+    type: [String],
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+  },
+  celular: {
+    type: String,
+    trim: true,
+    lowercase: true,
+  },
+  telefono: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  fechaCreacion: {
+    type: Date,
+    default: Date.now(),
+  },
+  paginaWeb: {
+    type: String,
+    trim: true,
+  },
+  nombre: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+  },
+  numeroIdentificacionTributario: {
+    type: String,
+    trim: true,
+  },
+  regimenSimplificado: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Proveedor = mongoose.model("Proveedor", proveedorSchema);
+
+const noUpdatable = ["__v"];
+
+Tercero.fieldsNotAllowedUpdates = (body) => {
+  const updates = Object.keys(body);
+
+  // Sirve para obtener los campos del modelo
+  let allowedUpdates = Object.keys(Tercero.schema.paths);
+
+  // Deja los campos que no queremos moficiar
+  allowedUpdates = allowedUpdates.filter(
+    (update) => !noUpdatable.includes(update)
+  );
+  const isValidOp = updates.every((update) => allowedUpdates.includes(update));
+  // console.log(updates);
+  return isValidOp;
+};
+
+module.exports = Proveedor;

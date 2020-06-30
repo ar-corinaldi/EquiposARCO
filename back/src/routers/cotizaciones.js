bbce501de9eadf6106f3b429ea67b664b6bf30e0
Diego <diego_gomezpolo@outlook.com>
2020-06-30 11:21:55 -0500

    fixes cotizaciones

--- back/src/routers/cotizaciones.js
+++ back/src/routers/cotizaciones.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const Cotizacion = require("../models/cotizacion-model");
+const Tarifa = require("../models/tarifa-model");
 
-const Router = new express.Router();
+const router = new express.Router();
 
-router.post("", async (req, resp) => {
+router.post("", async (req, res) => {
   const cotizacion = new Cotizacion(req.body);
   try {
     await cotizacion.save();
@@ -41,23 +42,63 @@ router.get("/:id", async (req, res) => {
 });
 
 router.patch("/:id", async (req, res) => {
-    try {
-      if (!Cotizacion.fieldsNotAllowedUpdates(req.body)) {
-        return res.status(400).send({ error: "Invalid updates" });
-      }
-  
-      const cotizacion = await Cotizacion.findByIdAndUpdate(req.params.id, req.body, {
+  try {
+    if (!Cotizacion.fieldsNotAllowedUpdates(req.body)) {
+      return res.status(400).send({ error: "Invalid updates" });
+    }
+
+    const cotizacion = await Cotizacion.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
         new: true,
         runValidators: true,
-      });
-      if (!cotizacion) {
-        return res.status(404).send();
       }
-      res.send(cotizacion);
-    } catch (e) {
-      res.status(400).send(e);
-      console.error("error", e);
+    );
+    if (!cotizacion) {
+      return res.status(404).send();
     }
-  });
+    res.send(cotizacion);
+  } catch (e) {
+    res.status(400).send(e);
+    console.error("error", e);
+  }
+});
 
-  module.exports = router;
+router.post("/:id/tarifasCotizadas", async (req, res) => {
+  try {
+    const cotizacion = await Cotizacion.findById(req.params.id);
+    const tarifa = new Tarifa(req.body);
+    await tarifa.save();
+
+    cotizacion.tarifasCotizadas.push(tarifa._id);
+    await cotizacion.save();
+  } catch (error) {
+    if (tarifa !== undefined) {
+      Tarifa.findByIdAndDelete(tarifa._id);
+    }
+  }
+});
+
+router.patch("/:id/tarifasCotizadas/:idTarifa", async (req, res) => {
+  try {
+    const cotizacion = await Cotizacion.findById(req.params.id);
+    console.log(req.params);
+    
+    const tarifa = await Tarifa.findById(req.params.idTarifa);
+    console.log("aquí");
+    
+    cotizacion.tarifasCotizadas.push(tarifa._id);
+    console.log("acá");
+    
+    await cotizacion.save();
+    // console.log(cotizacion);
+    res.json(cotizacion);
+    
+  } catch (e) {
+    res.status(400).send(e);
+    console.error("error", e);
+  }
+});
+
+module.exports = router;

d8e666e63f732c5c6d4ae2fe098d3c663676ef29
Diego <diego_gomezpolo@outlook.com>
2020-07-01 23:18:48 -0500

    css changes

--- front/src/components/FacturacionModule/EscogerBodega.js
+++ front/src/components/FacturacionModule/EscogerBodega.js
@@ -51,22 +51,6 @@ const useStyles = makeStyles((theme) => ({
     marginRight: 5,
     marginLeft: -2,
   },
-  color: {
-    width: 14,
-    height: 14,
-    flexShrink: 0,
-    borderRadius: 3,
-    marginRight: 8,
-    marginTop: 2,
-  },
-  text: {
-    flexGrow: 1,
-  },
-  close: {
-    opacity: 0.6,
-    width: 18,
-    height: 18,
-  },
 }));
 
 
@@ -163,7 +147,7 @@ export default function EscogerBodega(props) {
                 </div>
                 <CloseIcon
                   onClick={()=>{setPendingValue(null)}}
-                  className={classes.close}
+                  className= "close"
                   style={{ visibility: selected ? "visible" : "hidden" }}
                 />
               </React.Fragment>

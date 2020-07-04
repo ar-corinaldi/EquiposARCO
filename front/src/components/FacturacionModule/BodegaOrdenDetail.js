60012afe44e020d136f54ea9b1a5c46e650e30dd
Diego <diego_gomezpolo@outlook.com>
2020-07-03 20:35:32 -0500

    Equisita

--- front/src/components/FacturacionModule/BodegaOrdenDetail.js
+++ front/src/components/FacturacionModule/BodegaOrdenDetail.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./BodegaOrdenDetail.css";
-import holiday from '../utils/CacularTarifas'
+import holiday from '../utils/CacularTarifas';
+import CloseIcon from "@material-ui/icons/Close";
 
 function BodegaOrdenDetail(props) {
   const [bodegaSeleccionada, setBodegaSeleccionada] = props.bodegaSeleccionada;
+  const setPendingValue = props.setPendingValue;
 
   holiday();
 
@@ -12,12 +14,19 @@ function BodegaOrdenDetail(props) {
   } else {
     return (
       <div id="BodegaOrdenDetailWrapper">
+        <CloseIcon
+          className="closeIcon"
+          onClick={() => {
+            setPendingValue({});
+            setBodegaSeleccionada({});
+          }}
+        />
         <div className="bodegaHeader">
           <h2>{bodegaSeleccionada.nombreBodega} </h2>
         </div>
         <div className="descripcionBodega">
-          <p>{bodegaSeleccionada.municipio + ( bodegaSeleccionada.departamento? ", "+ bodegaSeleccionada.departamento : "")
-          + ", "+bodegaSeleccionada.pais + "."}</p>
+          <p>{bodegaSeleccionada.municipio + (bodegaSeleccionada.departamento ? ", " + bodegaSeleccionada.departamento : "")
+            + ", " + bodegaSeleccionada.pais + "."}</p>
           <p className="direccionBodega" >{bodegaSeleccionada.direccionBodega}</p>
         </div>
       </div>

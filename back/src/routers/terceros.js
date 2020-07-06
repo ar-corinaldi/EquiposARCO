3a3c70bbd916e2f501ce070387b0acd8720a9019
Isabela <im.sarmieento@uniandes.edu.co>
2020-07-06 12:53:46 -0500

    Orden Detail con estados logica en el front

--- back/src/routers/terceros.js
+++ back/src/routers/terceros.js
@@ -88,6 +88,7 @@ router.get("/terceros/bodegas", async (req, res) => {
  */
 router.get("/terceros/:id", async (req, res) => {
   try {
+    console.log("llega");
     const tercero = await Tercero.findById(req.params.id)
       .populate({
         path: "bodegas",
--- front/src/components/Orden/OrdenDetail.js
+++ front/src/components/Orden/OrdenDetail.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -9,14 +9,59 @@ import { useParams } from "react-router-dom";
 function OrdenDetail(props) {
   const { id, idB, idOr } = useParams();
 
+  const [orden, setOrden] = useState({});
+  const [statusOr, setStatus] = useState("");
+  const [tercero, setTercero] = useState({});
+  const [bodega, setBodega] = useState({});
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
+  const fetchInfo = async () => {
+    let res = await fetch("/terceros/" + id);
+    const terceroA = await res.json();
+    console.log("tercero", terceroA);
+    setTercero(terceroA);
+    let bodegaA;
+    terceroA.bodegas.forEach((bod) => {
+      if (bod._id.toString() === idB) {
+        bodegaA = bod;
+      }
+    });
+    console.log("bodega", bodegaA);
+    setBodega(bodegaA);
+    let ordenA;
+    bodegaA.ordenesActuales.forEach((or) => {
+      if (or._id.toString() === idOr) {
+        ordenA = or;
+        setStatus("En curso");
+        return;
+      }
+    });
+    if (!ordenA) {
+      bodegaA.ordenesPasadas.forEach((or) => {
+        if (or._id.toString() === idOr) {
+          ordenA = or;
+          setStatus("Terminada");
+          return;
+        }
+      });
+    }
+    console.log("orden", ordenA);
+    console.log("status", statusOr);
+    setOrden(ordenA);
+  };
+
   return (
     <Container fluid>
       <Row>
         <Col>
           <div className="orden-wrapper">
             <h3 className="page-title-orden">Orden No. {idOr}</h3>
-            <p>Tercero : {id} </p>
-            <p>Bodega : {idB}</p>
+            <p>Tercero : {tercero.nombre} </p>
+            <p>Bodega : {bodega.nombreBodega}</p>
+            <p>Status : {statusOr} </p>
           </div>
         </Col>
       </Row>

b50304ada729a5c35a5ff52499818a27b505432e
Allan <ar.corinaldi@uniandes.edu.co>
2020-07-28 11:35:24 -0500

    Manejo de errores withFormHandling y Toast y equipo model edited

--- front/src/components/Toast.js
+++ front/src/components/Toast.js
@@ -4,11 +4,11 @@ import "react-toastify/dist/ReactToastify.css";
 toast.configure();
 /**
  *
- * @param {errores a renderizar, debe ser un arreglo} errors arreglo de errores ["error1","error2"]
+ * @param {errores a renderizar, debe ser un arreglo} messages arreglo de errores ["error1","error2"]
  * @param {define si se debe cerrar o no automaticamente} autoClose false, numero o true (3000 segs)
  * @param {estado enviado del http por el servidor y lo mapea} status success: >=200, warn: >=400, error>=500
  */
-function Toast(errors, autoClose = 3000, status) {
+function Toast(messages, autoClose = 3000, status) {
   if (status) {
     status =
       status >= 400 && status < 500
@@ -19,14 +19,23 @@ function Toast(errors, autoClose = 3000, status) {
   }
   status = status || "warn";
   try {
-    return errors.map((err) =>
-      toast[status](err, {
+    if (Array.isArray(messages)) {
+      return messages.map((mes) =>
+        toast[status](mes, {
+          position: toast.POSITION.BOTTOM_RIGHT,
+          autoClose: autoClose,
+        })
+      );
+    } else if (typeof messages === "string") {
+      return toast[status](messages, {
         position: toast.POSITION.BOTTOM_RIGHT,
         autoClose: autoClose,
-      })
-    );
+      });
+    } else {
+      throw new Error("Se ha generado un error");
+    }
   } catch (e) {
-    return toast[status]("Se ha generado un error", {
+    return toast.error("Se ha generado un error", {
       position: toast.POSITION.BOTTOM_RIGHT,
     });
   }

337dd57a97561f9a5cf666a6748497ceadad3916
Isabela <im.sarmieento@uniandes.edu.co>
2020-08-06 18:02:33 -0500

    Pdf con svg,png y pdf 2 librerias

--- front/src/components/Imprimir/InventarioPorOrden/InfoOrden.js
+++ front/src/components/Imprimir/InventarioPorOrden/InfoOrden.js
@@ -1,60 +1,56 @@
 import React from "react";
 import TarifaRow from "./TarifaRow";
+import html2canvas from "html2canvas";
+import jsPDF from "jspdf";
 
 function InfoOrden(props) {
   const orden = props.orden;
   const tarifas = orden && orden.tarifasDefinitivas;
 
-  function getPDF() {
-    return fetch(`/your-pdf-endpoint`, {
-      responseType: "arraybuffer",
-      headers: {
-        Accept: "application/pdf",
-      },
-    });
-  }
+  const input = document.getElementById("to-print");
 
-  const savePDF = () => {
-    return getPDF() // API call
-      .then((response) => {
-        const blob = new Blob([response.data], { type: "application/pdf" });
-        const link = document.createElement("a");
-        link.href = window.URL.createObjectURL(blob);
-        link.download = `your-file-name.pdf`;
-        link.click();
-      })
-      .catch((err) => console.log(err));
+  const createPdf = () => {
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, "PNG", 0, 0);
+      pdf.save("download.pdf");
+    });
   };
 
   return (
-    <div className="orden-wrapper" id="orden-equipos-wrapper">
-      <h4 className="page-title-orden">Inventario Orden {orden.codigo}</h4>
-      <div className="table-responsive">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Item</th>
-              <th>Cantidad</th>
-              <th>Valor</th>
-              <th>Tipo de Cobro</th>
-              <th>Periodo Cobro</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tarifas &&
-              tarifas.map((tarifaAgrupada, index) => (
-                <TarifaRow
-                  key={index}
-                  tarifasPorEquipo={tarifaAgrupada.tarifasPorEquipo}
-                  index={index}
-                  opcion={1}
-                />
-              ))}
-          </tbody>
-        </table>
-        <button onClick={savePDF}>Save as PDF</button>
+    <div className="orden-wrapper-inventario" id="orden-equipos-wrapper">
+      <div id="to-print">
+        <h4 className="page-title-orden">Inventario Orden {orden.codigo}</h4>
+        <div className="table-responsive">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Item</th>
+                <th>Cantidad</th>
+                <th>Valor</th>
+                <th>Tipo de Cobro</th>
+                <th>Periodo Cobro</th>
+              </tr>
+            </thead>
+            <tbody>
+              {tarifas &&
+                tarifas.map((tarifaAgrupada, index) => (
+                  <TarifaRow
+                    key={index}
+                    tarifasPorEquipo={tarifaAgrupada.tarifasPorEquipo}
+                    index={index}
+                    opcion={1}
+                  />
+                ))}
+            </tbody>
+          </table>
+        </div>
       </div>
+      <button onClick={createPdf} className="button-print">
+        Save as PDF
+      </button>
     </div>
   );
 }
